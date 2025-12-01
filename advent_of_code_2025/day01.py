29e84f85ea31a40e3a7d5f91ee7740a87070fc92
Brian Dawn <brian.dawn@addium.io>
2025-12-01 13:12:38 -0800

    day01 part2

--- advent_of_code_2025/day01.py
+++ advent_of_code_2025/day01.py
@@ -2,24 +2,43 @@ import sys
 
 spot = 50
 part1 = 0
+part2 = 0
 
 for line in sys.stdin:
     line = line.strip()
 
     num = int(line[1:])
 
+
+    new_spot = spot
+
     if line[0] == "L":
-        spot -= num
+        new_spot -= num
     elif line[0] == "R":
-        spot += num
+        new_spot += num
     else:
         raise Exception("Unknown direction")
-    spot = spot % 100
 
-    if spot == 0:
+    if new_spot < 0 or new_spot >= 100:
+
+        if new_spot < 0 and spot != 0:
+            part2 += 1
+        part2 += int(abs(new_spot) / 100)
+
+    if new_spot == 0 and spot != 0:
+        part2 += 1
+    
+
+    new_spot = new_spot % 100
+    if new_spot == 0:
         part1 += 1
 
+    spot = new_spot
+    assert spot < 100 and spot >= 0
+
+
 print(part1)
+print(part2)
 
 
 
