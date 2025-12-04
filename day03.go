8d33b628ee6051572a12b9057ac67eaab80e8c72
Brian Dawn <brian.dawn@addium.io>
2025-12-03 22:28:15 -0800

    Day 3 part 2.

--- day03.go
+++ day03.go
@@ -6,10 +6,22 @@ import (
 	"os"
 )
 
+func digitsToInt(digits []int) int {
+	var final int
+
+	for _, n := range digits {
+		final = final*10 + n
+
+	}
+
+	return final
+}
 func main() {
 
 	scanner := bufio.NewScanner(os.Stdin)
 	part1 := 0
+	part2 := 0
+
 	for scanner.Scan() {
 		line := scanner.Text()
 
@@ -35,11 +47,52 @@ func main() {
 
 		}
 
-		maxJolt := firstMax * 10 + secondMax
+		maxJolt := firstMax*10 + secondMax
 		part1 += maxJolt
 
+		nums := []int{}
+		for _, ch := range line {
+			num := int(ch - '0')
+			nums = append(nums, num)
+		}
 
+
+		keptJolts := []int{}
+
+		// can keep 12 digits.
+		windowSize := len(nums) - 12 + 1
+		i := 0
+		for {
+			if i == len(nums) {
+				break
+			}
+
+			windowEnd := i + windowSize
+			windowEnd = min(len(nums), windowEnd)
+
+			// Find the biggest number and index inside the sliding window.
+			window := nums[i:windowEnd]
+			biggest := nums[i]
+			biggestIndex := 0
+			for j, windowNum := range window {
+				if biggest < windowNum {
+					biggest = windowNum
+					biggestIndex = j
+				}
+			}
+
+			keptJolts = append(keptJolts, biggest)
+			windowSize -= biggestIndex
+
+			i += 1 + biggestIndex
+		}
+
+		// Remove any trailing numbers if there were no good removal candidates.
+		keptJolts = keptJolts[0 : 12]
+		joltage := digitsToInt(keptJolts)
+		part2 += joltage
 	}
 
 	fmt.Println("part1:", part1)
+	fmt.Println("part2:", part2)
 }
